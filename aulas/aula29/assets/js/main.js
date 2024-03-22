139eb9d3a81688db15e0f5a3f77374d7cdf3d8ff
DanielCorte <122547533+DanielCorte@users.noreply.github.com>
2024-03-21 23:58:12 -0300

    continuação script

--- aulas/aula29/assets/js/main.js
+++ aulas/aula29/assets/js/main.js
@@ -4,4 +4,14 @@ function meuEscopo() {
 
     const weight = form.querySelector('.weight');
     const height = form.querySelector('.height');
+
+    function bmi(weight, height) {
+        if weight = Number {
+            if height = Number
+        }
+        const bmi = weight / (height * height)
+        return bmi;
+    }
+
+
 }
